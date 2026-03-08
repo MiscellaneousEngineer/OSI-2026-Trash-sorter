a96e17840330e58fb3363f74c6f55a20be8271e3
MiscellaneousEngineer <140086246+MiscellaneousEngineer@users.noreply.github.com>
2026-03-08 20:37:26 +0100

    Fixed frequency counter
    Figured out how to use PIO state machines and got it to work

--- CODE/FRQ_counter/FRQ_counter.c
+++ CODE/FRQ_counter/FRQ_counter.c
@@ -3,30 +3,32 @@
 #include "hardware/pio.h"
 #include "freq_counter.pio.h"
 
-#define INPUT_PIN 15
+#define FRQPIN 15
 
+uint T2 = 0;
+uint T1 = 0;
+uint T = 0;
+uint F = 0;
 int main()
 {
     stdio_init_all();
 
+
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &freq_counter_program);
     uint sm = pio_claim_unused_sm(pio, true);
-
-    pio_sm_config c = freq_counter_program_get_default_config(offset);
-
-    sm_config_set_in_pins(&c, INPUT_PIN);
-    pio_sm_set_consecutive_pindirs(pio, sm, INPUT_PIN, 1, false);
-    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
-    pio_gpio_init(pio, INPUT_PIN);
-    pio_sm_init(pio, sm, offset, &c);
-    pio_sm_set_enabled(pio, sm, true);
-
+    freq_counter_program_init(pio, sm, offset, FRQPIN);
+    pio->txf[0] = 1000;
 
     while (true)
     {
-        uint32_t count = pio_sm_get_blocking(pio, sm);
+        T1 = pio_sm_get_blocking(pio, sm);
+        sleep_ms(10);
+        T2 = pio_sm_get_blocking(pio, sm);
+        T = T1-T2;
+        F = T/0.01;
+        printf("Frequency : %u\n", F);
 
-        printf("Edge batch received: %u\n", count);
+        
     }
 }
