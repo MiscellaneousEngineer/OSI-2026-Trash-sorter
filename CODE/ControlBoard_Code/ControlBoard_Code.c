e4797dba41295ec2d08b0915c1179f1c81b158f8
MiscellaneousEngineer <140086246+MiscellaneousEngineer@users.noreply.github.com>
2026-03-08 21:05:14 +0100

    Implemented frequency code to main controller
    +added screen output

--- CODE/ControlBoard_Code/ControlBoard_Code.c
+++ CODE/ControlBoard_Code/ControlBoard_Code.c
@@ -4,32 +4,44 @@
 #include "hardware/pio.h"
 #include "hardware/uart.h"
 
-
-
 #include "freq_counter.pio.h"
-
 #include "tft.h"
 
-#define FREQ_PIN 15
-#define WINDOW_MS 100
+#define FRQPIN 15
+#define WINDOW_MS 10
 
-uint pulsecount;
-uint FRQ = 1;
-
-uint T;
+uint T2 = 0;
+uint T1 = 0;
+uint T = 0;
+uint F = 0;
 
 int main()
 {
 
     stdio_init_all();
 
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &freq_counter_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    freq_counter_program_init(pio, sm, offset, FRQPIN);
+
     tft_init();
     tft_fill(0);
 
     while (true)
     {
+        T1 = pio_sm_get_blocking(pio, sm);
+        sleep_ms(10);
+        T2 = pio_sm_get_blocking(pio, sm);
+        T = T1 - T2;
+        F = T / 0.01;
+        printf("Frequency : %u\n", F);
 
         tft_swap_sync();
-        tft_draw_string(1,1,100,"TEST");
+
+        char buffer[16];
+        snprintf(buffer, sizeof(buffer), "%d", F);
+          tft_fill(0);
+        tft_draw_string(20, 1, 100, buffer);
     }
 }
