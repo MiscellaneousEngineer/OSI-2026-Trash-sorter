535b13e47010fe5f8930387526736f2478241840
MiscellaneousEngineer <140086246+MiscellaneousEngineer@users.noreply.github.com>
2026-03-12 10:52:02 +0100

    Fix graphfreq function and improve square wave plotting

--- CODE/ControlBoard_Code/ControlBoard_Code.c
+++ CODE/ControlBoard_Code/ControlBoard_Code.c
@@ -15,34 +15,53 @@ uint T1 = 0;
 uint T = 0;
 uint F = 0;
 
-void graphfreq(uint16_t x, uint16_t y, uint16_t x color,uint16_t freq){
+void graphfreq(uint16_t x, uint16_t y, uint16_t x color,uint16_t freq){ //generates a graphic image of a perfect square wave of a given frequency
+//NOT A LOGIC ANALYSER
 uint localT;
 localT=1/freq;
 int plot[];
 bool state = 1;
-    
+uint index = 0;
+
+
 if(freq>1000000)//MHZ operation
 {//screen witdh is equal to 128 µS in MHZ mode
-localT = localT * 1000000; //convert from S to µs
-for(i = 127,i != 0, i--){
-
-    for(i = localT,i !=0,i--)
+localT = localT * 1000000; //convert from S to µs 
+while (index < 128) { //builds an image of the square wave in the plot array, over 128 pixels (index) 
+    for(i = localT,i !=0,i--) //build one T
     {
-        
+        plot[index] = state; 
+        index ++;
+        if (index = 128) break; //Exit for loop 
     }
-
+    bool = !bool; //switch state
 }
 
     
-}elif(freq>1000)//KHZ mode
-{
-
-}else//HZ mode
-{
-    
+}elif(freq>1000){//KHZ mode
+    localT = localT * 1000; //convert from S to ms 
+while (index < 128) { //builds an image of the square wave in the plot array, over 128 pixels (index) 
+    for(i = localT,i !=0,i--) //build one T
+    {
+        plot[index] = state; 
+        index ++;
+        if (index = 128) break; //Exit for loop 
+    }
+    bool = !bool; //switch state
+}
+}
+//Draw square wave from generated array and apply offsets
+index = 0;
+while (index != 128){
+if(plot[index]){//Draw high states
+    tft_draw_pixel(x + index, y, color);//draw high state
+}elif (!plot[index]){
+    tft_draw_pixel(x + index, y-20,color); //draw low state
+}
+index++;    
+}
 }
 
-}
 int main()
 {
 
